b3fe8d21e584977d58ff5004bc504aa706db7b24
Geovane Soares <geovane_soares01@outlook.com>
2025-11-05 00:21:25 -0300

    Banco de dados configurados com SQLAlchemy e modelos criados

--- gerenciamento/app/__init__.py
+++ gerenciamento/app/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flasgger import Swagger
+import os
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+
+    db_path = os.path.join(os.path.dirname(__file__), "..", "gerenciamento.db")
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{db_path}"
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
     # Configuração basica do Swagger
     app.config['SWAGGER'] = {
@@ -10,6 +19,12 @@ def create_app():
         'uiversion': 3
     }
     Swagger(app)
+
+    db.init_app(app)
+
+    with app.app_context():
+        from .models import Aluno, Professor, Turma
+        db.create_all()
 
     #Importar e registra as rotas
     from .routes import bp 
---
+++ gerenciamento/app/models.py
@@ -0,0 +1,18 @@
+from . import db
+
+class Aluno(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+class Professor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(120), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    area = db.Column(db.String(120), nullable=False)
+
+class Turma(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(120), nullable=False)
+    descricao = db.Column(db.String(255), nullable=True)
+    
